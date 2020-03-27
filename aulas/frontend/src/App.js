fca95005b7e8728fb6bb069b62269d1524cd529a
Pedro Ramos <ramosct70@hotmail.com>
2020-03-26 22:08:10 -0300

    Atualização frontend

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,25 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './global.css';
 
-// useState => para renderizar os elementos em tela quando que uma váriavel for alterada
-
-import Header from './Header';
-
-// JSX => JS + XML
+import Routes from './routes';
 
 function App() {
-  const [counter, setCounter] = useState(0); // counter => valor, setCounter => função para alterar counter
-
-  function increment() {
-    setCounter(counter + 1);
-
-    console.log(counter);
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Routes />
   );
 }
 
