b9a20a9763a3904aaf5d9a5e763b964e930b8bad
Pedro Ramos <ramosct70@hotmail.com>
2020-03-27 22:00:50 -0300

    mobile

---
+++ aulas/mobile/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Routes from './src/routes';
+
+// View => div, header, footer etc.
+// Text => strong, h1, p etc.
+
+export default function App() {
+  return (
+    <View>
+      <Text>Be The Hero</Text>
+    </View>
+  );
+}
+
